8c503b3034e8a912773a41896a712d3d75e115e3
Tanvir Hossain <tanvir@disystem.net>
2025-08-26 09:20:06 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Welcome to E-Commerce Mini 🛒</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        Let’s start building your store!
+      </p>
+    </main>
+  );
+}
